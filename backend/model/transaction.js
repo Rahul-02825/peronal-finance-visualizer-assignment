057936a72b2ef4276b08c4b166824231bbfc2c3a
Rahul-02825 <rahul2825.kannan@gmail.com>
2025-04-18 00:01:59 +0530

    category added stage2 cooking

--- backend/model/transaction.js
+++ backend/model/transaction.js
@@ -1,9 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const transactionSchema = new mongoose.Schema({
-    amount:Number,
-    date:Date,
-    description:String
-},{timestamps:true})
+const transactionSchema = new mongoose.Schema(
+  {
+    amount: Number,
+    date: Date,
+    description: String,
+    category: {
+      type: String,
+      enum: ['Food', 'Transport', 'Utilities', 'Entertainment', 'Health', 'Other'],
+      required: true
+    }
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Transaction',transactionSchema)
+module.exports = mongoose.model('Transaction', transactionSchema);
